16e1cb9c62f99fde71f0ee3d92d0f52c0c4b33aa
aline <delettrealine@gmail.com>
2023-11-09 21:41:48 +0100

    feat: create ApodDetails.tsx view component

---
+++ src/views/ApodDetails.tsx
@@ -0,0 +1,35 @@
+import {useEffect, useRef, useState} from 'react';
+import {Apod} from '../types/Apod.ts';
+import {useParams} from 'react-router-dom';
+import ApodExpandedCard from '../components/ApodExpandedCard.tsx';
+import Error from '../components/Error.tsx';
+import Loading from '../components/Loading.tsx';
+import {getAPOD} from '../api/apod.ts';
+import {dateSchema} from '../utils/Validation.ts';
+
+const ApodDetails = () => {
+  const {apodId} = useParams<string>();
+  const validatedApodId: Date = dateSchema.parse(apodId);
+  const apodDate = useRef(validatedApodId);
+
+  const [apod, setApod] = useState<Apod | undefined>(undefined)
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false)
+
+  useEffect(() => {
+    getAPOD(apodDate.current).catch((error) => {
+      setIsError(true);
+      throw error;
+    }).then((response: Apod) => setApod(response));
+    return setIsLoading(false);
+  }, []);
+
+  return (
+    <>
+      {isError ? <Error/> : !isLoading && apod ? <ApodExpandedCard apodDate={validatedApodId} apod={apod}/> :
+        <Loading/>}
+    </>
+  );
+};
+
+export default ApodDetails;
