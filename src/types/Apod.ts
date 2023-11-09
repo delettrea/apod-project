a8b8bac4548205473383169a4daa32cf8998d4e8
aline <delettrealine@gmail.com>
2023-11-09 21:22:55 +0100

    feat: create Apod type
    create Apod type to define apod picture

---
+++ src/types/Apod.ts
@@ -0,0 +1,11 @@
+export type Apod = {
+  copyright?: string,
+  date: string,
+  explanation: string,
+  hdurl: string,
+  url?: string,
+  media_type: 'image' | 'video',
+  service_version: string,
+  title: string,
+  thumbnail_url?: string,
+}
