bdaae3d9eeb60428003b4a788002dfbc14357a46
aline <delettrealine@gmail.com>
2023-11-09 22:04:06 +0100

    feat: create .env file and create vite-env.d.ts
    add .env file and create vite-env.d.ts to define all values

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="vite/client" />
+// / <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_NASA_API_KEY: string,
+  readonly VITE_NASA_API: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
