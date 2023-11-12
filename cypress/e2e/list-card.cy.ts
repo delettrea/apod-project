228a4ba635bb55a9c39993b2700bc59a4de955d2
aline <delettrealine@gmail.com>
2023-11-12 14:47:52 +0100

    test: add list-card.cy.ts e2e test

---
+++ cypress/e2e/list-card.cy.ts
@@ -0,0 +1,22 @@
+describe("test apod consultation", () => {
+  beforeEach(() => {
+    cy.intercept('GET', "https://api.nasa.gov/planetary/apod*", {fixture: "apod-list.json"})
+    cy.visit("/");
+  })
+
+  it("should load at least one apod cards", () => {
+    cy.get("a").should("have.length.gt",0);
+  });
+
+  it("should have clickable cards who redirect to card's url", () => {
+    cy.get("a:first").click();
+    cy.url().should('include','/apod/20231111');
+  });
+
+  it("should have return button to see all cards in card details", () => {
+    cy.get("a:first").click();
+    cy.get("button").click();
+    cy.url().should('include','/');
+    cy.get("a").should("have.length.gt",0);
+  });
+})
