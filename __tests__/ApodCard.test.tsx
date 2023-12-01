5e0ebd22ca112100b6eb39d5cc875242bf225545
aline <delettrealine@gmail.com>
2023-12-01 21:44:51 +0100

    test: edit ApodCard.test.tsx with new date and data

--- __tests__/ApodCard.test.tsx
+++ __tests__/ApodCard.test.tsx
@@ -1,15 +1,16 @@
 import {describe, expect, it} from 'vitest';
 import {render, screen} from '@testing-library/react';
-import {apodFakeData, apodVideoFakeData} from './fakeData';
-import ApodCard from '../src/components/ApodCard';
+import {apodFakeData, apodVideoFakeData} from './_data/fakeApodData';
+import ApodCard from '../src/components/apod/ApodCard';
 import '@testing-library/jest-dom'
 import { BrowserRouter as Router } from 'react-router-dom';
-import {formatApodIdToDate, formatStringDateToId} from '../src/utils/Date';
+import {formatApodDateToDate} from '../src/utils/date';
+import {Apod} from '../src/types/Apod';
 
 describe('ApodCard component', () => {
-    it('renders with correct data', () => {
-      const apodData= apodFakeData;
-      const apodDataDate: Date = formatApodIdToDate(formatStringDateToId(apodData.date));
+    it('renders with correct _data', () => {
+      const apodData: Apod= apodFakeData;
+      const apodDataDate: Date = formatApodDateToDate(apodData.date);
 
       render(
         <Router>
@@ -17,15 +18,15 @@ describe('ApodCard component', () => {
         </Router>
       );
 
-      expect(screen.getAllByAltText(apodData.title)).toMatchFileSnapshot("snapshot/PartialLunarItaly_Mezzio_1080.jpg");
+      expect(screen.getAllByAltText(apodData.title)).toMatchFileSnapshot("_snapshot/PartialLunarItaly_Mezzio_5524.jpg");
       expect(screen.getByText(apodData.title)).toBeInTheDocument();
       expect(screen.getByRole('heading')).toHaveTextContent(apodData.title);
       expect(screen.getByText(`Date : ${apodDataDate.toLocaleDateString()}`)).toBeInTheDocument();
     });
 
     it('renders video for media_type "video"', () => {
-      const apodData = apodVideoFakeData;
-      const apodDataDate: Date = formatApodIdToDate(formatStringDateToId(apodData.date));
+      const apodData: Apod = apodVideoFakeData;
+      const apodDataDate: Date = formatApodDateToDate(apodData.date);
 
       render(
         <Router>
